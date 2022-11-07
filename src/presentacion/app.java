779585e39b3ca2d58933faff46202ec95dfd83cf
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-07 17:34:15 -0500

    Cambios de Gian

--- src/presentacion/app.java
+++ src/presentacion/app.java
@@ -6,6 +6,7 @@ package presentacion;
  */
 public class app {
   public static void main(String[] args) {
-      System.out.println("Holaaaa");
+    System.out.println("Holaaaa");
+    System.out.println("Adios");
   }
 }
