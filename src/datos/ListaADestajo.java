18192eb04a2dc93e9ffd6fb1b39dac72c977449f
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-14 00:44:38 -0500

    Implementando clase ListaADestajo

--- src/datos/ListaADestajo.java
+++ src/datos/ListaADestajo.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package datos;
 
 import entidades.*;
 
-/**
- *
- * @author Verastegui
- */
 public class ListaADestajo {
     public final static int TAM = 10;
     private static ADestajo listaA[] = new ADestajo[TAM];
