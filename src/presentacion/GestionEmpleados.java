15111723468c6a2ffce50d2189af1499cee1ce17
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-14 17:55:34 -0500

    Funcion 4 Daniel

--- src/presentacion/GestionEmpleados.java
+++ src/presentacion/GestionEmpleados.java
@@ -13,7 +13,7 @@ public class GestionEmpleados {
                           1. Registrar Empleados contratados 
                           2. Registrar Empleados a Destajo
                           3. 
-                          4. 
+                          4. Registrar las ventas realizadas por los empleados a destajo
                           5. 
                           6.
                           7.
@@ -32,9 +32,11 @@ public class GestionEmpleados {
   public static void main(String[] args) {
     Empleado empleado = new Empleado();
     String dni, nombre, apellido;
-    int genero, diaI, mesI, añoI, diaN, mesN, añoN;
+    int genero, diaI, mesI, añoI, diaN, mesN, añoN, diaV, mesV, añoV;
     float salario;
-    Fecha fechaNacimiento, fechaIngreso;
+    Fecha fechaNacimiento, fechaIngreso, fechaVenta;
+    Cliente clienteA;
+    Venta ventaA;
     int opcion;
     System.out.println("\nDatos del equipo:\n" + empleado.toString());
     do {
@@ -102,6 +104,42 @@ public class GestionEmpleados {
           System.out.println("\nDatos del equipo:\n" + ADestajo.toString());
           ListaADestajo.setADestajo(ADestajo);
           break;
+        case 4:
+          System.out.println("Digite DNI del Empleado a destajo: ");
+          dni = sc.nextLine();
+          int v = 0;
+          int p = 0;
+          for(int i = 0; i<=ListaADestajo.getPosicion();i++){
+            if(ListaADestajo.getADestajo(i).getDni().compareTo(dni)==0){
+              v = 1;
+              p = i;
+            }
+          }
+          if(v == 1) {
+            System.out.println("Digite fecha de venta\n\tDia : ");
+            diaV = sc.nextInt();
+            System.out.print("\tMes: ");
+            mesV = sc.nextInt();
+            System.out.print("\tAnio: ");
+            añoV = sc.nextInt();
+            fechaVenta = new Fecha(diaV, mesV, añoV);
+            sc.nextLine();
+            System.out.println("Digite datos de cliente\n\tDNI : ");
+            String dniCliente = sc.nextLine();
+            System.out.print("\tNombre: ");
+            nombre = sc.nextLine();
+            System.out.print("\tApellido: ");
+            apellido = sc.nextLine();
+            clienteA = new Cliente(dniCliente,nombre,apellido);
+            System.out.println("\tDigite monto de venta: ");
+            float monto = sc.nextFloat();
+            System.out.println("Digite numero de documento de venta: ");
+            String numDoc = sc.nextLine();
+            ventaA = new Venta(numDoc , fechaVenta , clienteA, monto);
+            ListaADestajo.getADestajo(p).setVenta(ventaA);
+          } else {
+            System.out.println("Empleado a destajo no encontrado ");
+          }
       }
     }while(opcion != 8);
   }
