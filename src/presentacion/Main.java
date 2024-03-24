d68a09f133b85fa890e3c074b309043c4dce7ee7
gians1821 <gian.samana.1801@gmail.com>
2024-03-24 18:14:09 -0500

    feat: registrar empleado frame added

---
+++ src/presentacion/Main.java
@@ -0,0 +1,12 @@
+package presentacion;
+
+
+import presentacion.RegistroEmpleadoFrame;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        new RegistroEmpleadoFrame().setVisible(true);
+    }
+    
+}
